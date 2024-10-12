10edb565661822f6bc78d40ac7380bef9d617430
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-12 14:59:37 -0300

    Adicionar mensagem

---
+++ mensagem/src/ComputadorPedrinho.java
@@ -0,0 +1,8 @@
+public class ComputadorPedrinho {
+    public static void main(String[]args){
+        MSNMEssenger msn = new MSNMenssenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
