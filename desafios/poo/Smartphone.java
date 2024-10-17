e7da37de6bb3bda4ab3ec1269778366f25376808
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-17 14:52:49 -0300

    melhora no código

--- desafios/poo/Smartphone.java
+++ desafios/poo/Smartphone.java
@@ -1,3 +1,24 @@
+
+// Interface para Reprodutor Musical
+interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
+// Interface para Aparelho Telefônico
+interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
+
+// Interface para Navegador na Internet
+interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
 // Classe Smartphone implementando todas as funcionalidades
 public class Smartphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
 
