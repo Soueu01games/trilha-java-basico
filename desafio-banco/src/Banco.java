8d48e2cc19a7d6ed790cd86dee6405d1426ad30b
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-10-19 13:07:09 -0300

    adicionando clientes

--- desafio-banco/src/Banco.java
+++ desafio-banco/src/Banco.java
@@ -1,18 +1,44 @@
+import java.util.ArrayList;
 import java.util.List;
+
 public class Banco {
     private String nome;
     private List<Conta> contas;
-    public String getNome(){
+    private List<Cliente> clientes; // Nova lista para armazenar os clientes
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+        this.clientes = new ArrayList<>();
+    }
+
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public List<Conta> getContas(){
+    public List<Conta> getContas() {
         return contas;
     }
-    public void setContas(List<Conta> contas){
+
+    public void setContas(List<Conta> contas) {
         this.contas = contas;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+    }
+
+    public void listarClientes() {
+        System.out.println("Clientes do banco:");
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente.getNome());
+        }
     }
 }
--- desafio-banco/src/main.java
+++ desafio-banco/src/main.java
@@ -1,16 +1,51 @@
 public class main {
-    public static void main(String [] args){
-        Cliente emanuela =new Cliente();
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+        banco.setNome("Banco Digital");
+
+        //Adicionando novos clientes
+        Cliente emanuela = new Cliente();
         emanuela.setNome("Emanuela");
+        banco.adicionarCliente(emanuela);
 
+        Cliente joao = new Cliente();
+        joao.setNome("João");
+        banco.adicionarCliente(joao);
 
+        Cliente maria = new Cliente();
+        maria.setNome("Maria");
+        banco.adicionarCliente(maria);
+
+        //Criando as contas dos clienes
         Conta cc = new ContaCorrente(emanuela);
         Conta poupanca = new ContaPoupanca(emanuela);
-        
-        cc.depositar(100);
-        cc.transferir(100, poupanca);
 
+        Conta cc1 = new ContaCorrente(joao);
+        Conta poupanca1 = new ContaPoupanca(joao);
+
+        Conta cc2 = new ContaCorrente(maria);
+        Conta poupanca2 = new ContaPoupanca(maria);
+
+        //Simulando transações
+        cc.depositar(300);
+        cc.transferir(100, cc2);
+
+        cc1.sacar(25);
+
+        cc2.depositar(200);
+        cc2.transferir(100, poupanca2);
+
+        //Imprimindo resultados
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
+
+        cc1.imprimirExtrato();
+        poupanca1.imprimirExtrato();
+
+        cc2.imprimirExtrato();
+        poupanca2.imprimirExtrato();
+
+        banco.listarClientes();
     }
 }
+
